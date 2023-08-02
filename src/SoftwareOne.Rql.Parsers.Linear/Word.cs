449b60d4efb95f83a63899277376aa49822d6b75
natan <natan@podbielski.it>
2023-08-02 09:01:26 +0200

    fix(*): fix few typos + smaller code improvements

--- src/SoftwareOne.Rql.Parsers.Linear/Word.cs
+++ src/SoftwareOne.Rql.Parsers.Linear/Word.cs
@@ -4,17 +4,17 @@
     {
         public ReadOnlyMemory<char> Text { get; set; }
         public int WordStart { get; set; }
-        public int WordLenght { get; set; }
+        public int WordLength { get; set; }
 
         public char? WrapSymbol { get; set; }
         public int? WrapStart { get; set; }
         public int? WrapEnd { get; set; }
         public GroupType GroupType { get; set; }
-        public List<int> Delimeters { get; set; }
+        public List<int> Delimiters { get; set; }
 
-        public override readonly string ToString() => Text.Slice(WordStart, WordLenght).ToString();
+        public readonly override string ToString() => Text.Slice(WordStart, WordLength).ToString();
 
         public static Word Make(ReadOnlyMemory<char> text, int start)
-            => new() { Text = text, WordStart = start, Delimeters = new List<int>() };
+            => new() { Text = text, WordStart = start, Delimiters = new List<int>() };
     }
 }
