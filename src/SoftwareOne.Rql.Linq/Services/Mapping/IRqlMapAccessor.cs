461aa8d4663208105973af740dbca861f8d8ec17
Damir Usmanov <damir.usmanov@softwareone.com>
2025-01-09 09:55:28 +0000

    Merged PR 132895: [MPT-5456] IRqlMapAccessor non generic implementation added
    IRqlMapAccessor non generic implementation added
    
    Pre-requisites for Jira:
    https://softwareone.atlassian.net/browse/MPT-5456

--- src/SoftwareOne.Rql.Linq/Services/Mapping/IRqlMapAccessor.cs
+++ src/SoftwareOne.Rql.Linq/Services/Mapping/IRqlMapAccessor.cs
@@ -6,12 +6,15 @@ namespace SoftwareOne.Rql;
 public interface IRqlMapAccessor
 {
     RqlMapDescriptor Get<TFrom, TTo>();
+
+    RqlMapDescriptor Get(Type typeFrom, Type typeTo);
 }
 
 internal class RqlMapAccessor(IEntityMapCache mapCache) : IRqlMapAccessor
 {
     public RqlMapDescriptor Get<TFrom, TTo>()
-    {
-        return new RqlMapDescriptor(mapCache.Get(typeof(TFrom), typeof(TTo)));
-    }
+        => Get(typeof(TFrom), typeof(TTo));
+
+    public RqlMapDescriptor Get(Type typeFrom, Type typeTo)
+        => new(mapCache.Get(typeFrom, typeTo));
 }
